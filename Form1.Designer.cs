f72b4a1144e0910379585509938b714dfc87be2a
Tikky1 <efekankul5161@gmail.com>
2024-11-25 00:21:46 +0300

    25.11.2024 00:21

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace MusicApplication
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace MusicApplication
 {
     partial class Form1
     {
@@ -11,10 +14,13 @@
         ///Kullanılan tüm kaynakları temizleyin.
         /// </summary>
         ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        ///
         protected override void Dispose(bool disposing)
         {
+
             if (disposing && (components != null))
             {
+
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -36,6 +42,10 @@
             this.songImage = new System.Windows.Forms.PictureBox();
             this.timeSlider = new System.Windows.Forms.TrackBar();
             this.songLabel = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.openButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBox = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.volumeSlider)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.songImage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.timeSlider)).BeginInit();
@@ -43,41 +53,39 @@
             // 
             // backButton
             // 
-            this.backButton.Location = new System.Drawing.Point(89, 450);
-            this.backButton.Margin = new System.Windows.Forms.Padding(4);
+            this.backButton.Location = new System.Drawing.Point(73, 370);
             this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(100, 28);
+            this.backButton.Size = new System.Drawing.Size(75, 23);
             this.backButton.TabIndex = 0;
             this.backButton.Text = "Back";
             this.backButton.UseVisualStyleBackColor = true;
-            this.backButton.Click += new System.EventHandler(this.button1_Click);
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
             // 
             // pauseButton
             // 
-            this.pauseButton.Location = new System.Drawing.Point(197, 450);
-            this.pauseButton.Margin = new System.Windows.Forms.Padding(4);
+            this.pauseButton.Location = new System.Drawing.Point(154, 370);
             this.pauseButton.Name = "pauseButton";
-            this.pauseButton.Size = new System.Drawing.Size(100, 28);
+            this.pauseButton.Size = new System.Drawing.Size(75, 23);
             this.pauseButton.TabIndex = 1;
             this.pauseButton.Text = "Pause/Start";
             this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
             // 
             // nextbutton
             // 
-            this.nextbutton.Location = new System.Drawing.Point(305, 450);
-            this.nextbutton.Margin = new System.Windows.Forms.Padding(4);
+            this.nextbutton.Location = new System.Drawing.Point(235, 370);
             this.nextbutton.Name = "nextbutton";
-            this.nextbutton.Size = new System.Drawing.Size(100, 28);
+            this.nextbutton.Size = new System.Drawing.Size(75, 23);
             this.nextbutton.TabIndex = 2;
             this.nextbutton.Text = "Next";
             this.nextbutton.UseVisualStyleBackColor = true;
+            this.nextbutton.Click += new System.EventHandler(this.nextbutton_Click);
             // 
             // volumeSlider
             // 
-            this.volumeSlider.Location = new System.Drawing.Point(168, 388);
-            this.volumeSlider.Margin = new System.Windows.Forms.Padding(4);
+            this.volumeSlider.Location = new System.Drawing.Point(134, 319);
             this.volumeSlider.Name = "volumeSlider";
-            this.volumeSlider.Size = new System.Drawing.Size(149, 56);
+            this.volumeSlider.Size = new System.Drawing.Size(112, 45);
             this.volumeSlider.TabIndex = 3;
             // 
             // songImage
@@ -87,38 +95,66 @@
             this.songImage.ErrorImage = ((System.Drawing.Image)(resources.GetObject("songImage.ErrorImage")));
             this.songImage.Image = ((System.Drawing.Image)(resources.GetObject("songImage.Image")));
             this.songImage.InitialImage = ((System.Drawing.Image)(resources.GetObject("songImage.InitialImage")));
-            this.songImage.Location = new System.Drawing.Point(122, 9);
+            this.songImage.Location = new System.Drawing.Point(92, 7);
             this.songImage.Margin = new System.Windows.Forms.Padding(0);
             this.songImage.Name = "songImage";
-            this.songImage.Size = new System.Drawing.Size(256, 256);
+            this.songImage.Size = new System.Drawing.Size(192, 208);
             this.songImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.songImage.TabIndex = 4;
             this.songImage.TabStop = false;
             // 
             // timeSlider
             // 
-            this.timeSlider.Location = new System.Drawing.Point(155, 486);
-            this.timeSlider.Margin = new System.Windows.Forms.Padding(4);
+            this.timeSlider.Location = new System.Drawing.Point(112, 399);
             this.timeSlider.Name = "timeSlider";
-            this.timeSlider.Size = new System.Drawing.Size(191, 56);
+            this.timeSlider.Size = new System.Drawing.Size(143, 45);
             this.timeSlider.TabIndex = 5;
             // 
             // songLabel
             // 
-            this.songLabel.AutoSize = true;
-            this.songLabel.Location = new System.Drawing.Point(211, 278);
-            this.songLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.songLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.songLabel.Location = new System.Drawing.Point(0, 443);
             this.songLabel.Name = "songLabel";
-            this.songLabel.Size = new System.Drawing.Size(79, 16);
+            this.songLabel.Size = new System.Drawing.Size(374, 13);
             this.songLabel.TabIndex = 6;
             this.songLabel.Text = "Song Name";
+            this.songLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk_1);
+            // 
+            // openButton
+            // 
+            this.openButton.Image = global::MusicApplication.Properties.Resources.Ekran_görüntüsü_2024_07_21_021455;
+            this.openButton.Location = new System.Drawing.Point(334, 12);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(20, 23);
+            this.openButton.TabIndex = 7;
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.listBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(135, 218);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(120, 91);
+            this.listBox.TabIndex = 8;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(488, 556);
+            this.ClientSize = new System.Drawing.Size(374, 456);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.openButton);
             this.Controls.Add(this.songLabel);
             this.Controls.Add(this.timeSlider);
             this.Controls.Add(this.songImage);
@@ -126,7 +162,7 @@
             this.Controls.Add(this.nextbutton);
             this.Controls.Add(this.pauseButton);
             this.Controls.Add(this.backButton);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Music App";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -147,6 +183,10 @@
         private System.Windows.Forms.PictureBox songImage;
         private System.Windows.Forms.TrackBar timeSlider;
         private System.Windows.Forms.Label songLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.ListBox listBox;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,20 +4,28 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave;
 
 namespace MusicApplication
 {
     public partial class Form1 : Form
     {
+        string content = "";
+        string filePath = @"C:%userprofile%\AppData\Local\Temp\songlist.txt";
+
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+        bool isPlaying = true;
+        string[] songs = null;
+        int currentSong = 0;
         public Form1()
         {
             InitializeComponent();
-
-
         }
         protected override void OnPaint(PaintEventArgs e)
         {
@@ -42,19 +50,118 @@ namespace MusicApplication
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
 
-        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+
+
+        private void openFileDialog1_FileOk_1(object sender, CancelEventArgs e)
         {
 
         }
+
+
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Bir klasör seçin";
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(filePath, folderBrowserDialog.SelectedPath);
+                    string selectedPath = folderBrowserDialog.SelectedPath;
+
+                    // Klasördeki MP3 dosyalarını listele
+                    songs = Directory.GetFiles(selectedPath, "*.mp3");
+
+                    // ListBox'a ekle
+                    listBox.Items.Clear();
+                    
+                    foreach (var filePath in songs)
+                    {
+                        listBox.Items.Add(Path.GetFileName(filePath)); // Sadece dosya adını al
+                    }
+
+
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            if (isPlaying)
+            {
+                outputDevice.Play();
+            }
+            else
+            {
+                outputDevice.Stop();
+            }
+            isPlaying = !isPlaying;
+        }
+
+        private void nextbutton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currentSong = listBox.SelectedIndex;
+            Play();
+
+        }
+        public void Play()
+        {
+
+
+
+
+
+            if (outputDevice != null)
+            {
+                outputDevice.Dispose();
+            }
+            if (audioFile != null)
+            {
+                audioFile.Dispose();
+            }
+
+            audioFile = new AudioFileReader(songs[currentSong]);
+            outputDevice = new WaveOutEvent();
+            outputDevice.Init(audioFile);
+
+            songLabel.Text = $"Playing: {songs[currentSong].Substring(songs[currentSong].LastIndexOf(@"\")+1)}";
+        }
+
     }
+
+
 }
+
