6a695758e07f62cb2297cd3a241fe1f3c66bea52
Tikky1 <efekankul5161@gmail.com>
2024-11-24 21:01:43 +0300

    UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,27 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.backButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.nextbutton = new System.Windows.Forms.Button();
+            this.volumeSlider = new System.Windows.Forms.TrackBar();
+            this.songImage = new System.Windows.Forms.PictureBox();
+            this.timeSlider = new System.Windows.Forms.TrackBar();
+            this.songLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.songImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timeSlider)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // backButton
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(344, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Choose a Music";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.backButton.Location = new System.Drawing.Point(67, 366);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 0;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Location = new System.Drawing.Point(148, 366);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(75, 23);
+            this.pauseButton.TabIndex = 1;
+            this.pauseButton.Text = "Pause/Start";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            // 
+            // nextbutton
+            // 
+            this.nextbutton.Location = new System.Drawing.Point(229, 366);
+            this.nextbutton.Name = "nextbutton";
+            this.nextbutton.Size = new System.Drawing.Size(75, 23);
+            this.nextbutton.TabIndex = 2;
+            this.nextbutton.Text = "Next";
+            this.nextbutton.UseVisualStyleBackColor = true;
+            // 
+            // volumeSlider
+            // 
+            this.volumeSlider.Location = new System.Drawing.Point(126, 315);
+            this.volumeSlider.Name = "volumeSlider";
+            this.volumeSlider.Size = new System.Drawing.Size(112, 45);
+            this.volumeSlider.TabIndex = 3;
+            // 
+            // songImage
+            // 
+            this.songImage.Location = new System.Drawing.Point(102, 42);
+            this.songImage.Name = "songImage";
+            this.songImage.Size = new System.Drawing.Size(169, 145);
+            this.songImage.TabIndex = 4;
+            this.songImage.TabStop = false;
+            // 
+            // timeSlider
+            // 
+            this.timeSlider.Location = new System.Drawing.Point(116, 395);
+            this.timeSlider.Name = "timeSlider";
+            this.timeSlider.Size = new System.Drawing.Size(143, 45);
+            this.timeSlider.TabIndex = 5;
+            // 
+            // songLabel
+            // 
+            this.songLabel.AutoSize = true;
+            this.songLabel.Location = new System.Drawing.Point(158, 226);
+            this.songLabel.Name = "songLabel";
+            this.songLabel.Size = new System.Drawing.Size(60, 13);
+            this.songLabel.TabIndex = 6;
+            this.songLabel.Text = "SongName";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(366, 452);
+            this.Controls.Add(this.songLabel);
+            this.Controls.Add(this.timeSlider);
+            this.Controls.Add(this.songImage);
+            this.Controls.Add(this.volumeSlider);
+            this.Controls.Add(this.nextbutton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.backButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Music App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.volumeSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.songImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timeSlider)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,7 +126,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button nextbutton;
+        private System.Windows.Forms.TrackBar volumeSlider;
+        private System.Windows.Forms.PictureBox songImage;
+        private System.Windows.Forms.TrackBar timeSlider;
+        private System.Windows.Forms.Label songLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,38 @@ namespace MusicApplication
         public Form1()
         {
             InitializeComponent();
-            this.Text = "Mu";
-            label1.Text = "Bomba";
+
+
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // Çizim yüzeyi
+            Graphics graphics = e.Graphics;
+
+            // Gradyan fırça oluştur
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                this.ClientRectangle, // Gradyan alanı
+                Color.GreenYellow,           // Başlangıç rengi
+                Color.LimeGreen,            // Bitiş rengi
+                LinearGradientMode.ForwardDiagonal)) // Yön: Yatay
+            {
+                // Gradyanı doldur
+                graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
